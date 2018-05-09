981f04060e1f76b1cb7d7bd4c0d44da0bf6eef09
krishnazax <30795196+krishnazax@users.noreply.github.com>
2018-05-09 09:48:29 -0700

    Add files via upload

---
+++ AddMenu.java
@@ -0,0 +1,39 @@
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+import javax.swing.border.*;
+
+public class AddMenu
+{
+    JFrame frame;
+    ProblemDatabase problem;
+    
+    public AddMenu(ProblemDatabase problem)
+    {
+        this.problem = problem;
+        
+        JButton back = new JButton( "Back" );
+        back.setBounds( 10, 10, 50, 50 );
+        back.addActionListener( new BackButtonListener() );
+        
+        frame = new JFrame( "Add Menu" );
+        frame.setDefaultCloseOperation( JFrame.EXIT_ON_CLOSE );
+        Container c = frame.getContentPane();
+        frame.setLayout( null );
+        frame.setBounds( 0, 0, 800, 600 );
+        
+        c.add( back );
+        
+        frame.setResizable( false );
+        frame.setVisible( true );
+    }
+    
+    private class BackButtonListener implements ActionListener
+    {
+        public void actionPerformed( ActionEvent e )
+        {
+            MainMenu mainMenu = new MainMenu( problem );
+            frame.dispose();
+        }
+    }
+}
