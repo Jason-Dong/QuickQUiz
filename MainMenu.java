4d7cd7b306989b31ed524374c7bb14e1abe8b7aa
krishnazax <30795196+krishnazax@users.noreply.github.com>
2018-05-16 10:35:31 -0700

    Add files via upload

--- MainMenu.java
+++ MainMenu.java
@@ -14,27 +14,32 @@ public class MainMenu
         this.problem = problem;
         
         JButton QQMenu = new JButton( "Quick Quiz" );
-        QQMenu.setBounds( 200, 150, 400, 100 );
+        QQMenu.setBounds( 200, 125, 400, 75 );
         QQMenu.addActionListener( new QQButtonListener() );
 
         JButton trainMenu = new JButton( "Training" );
-        trainMenu.setBounds( 200, 250, 400, 100 );
+        trainMenu.setBounds( 200, 200, 400, 75 );
         trainMenu.addActionListener( new TrainButtonListener() );
 
         JButton addMenu = new JButton( "Add Problems" );
-        addMenu.setBounds( 200, 350, 400, 100 );
+        addMenu.setBounds( 200, 275, 400, 75 );
         addMenu.addActionListener( new AddButtonListener() );
 
         JButton removeMenu = new JButton( "Remove Problems" );
-        removeMenu.setBounds( 200, 450, 400, 100 );
+        removeMenu.setBounds( 200, 350, 400, 75 );
         removeMenu.addActionListener( new RemoveButtonListener() );
+        
+        JButton statsMenu = new JButton( "Statistics" );
+        statsMenu.setBounds( 200, 425, 400, 75 );
+        statsMenu.addActionListener( new StatButtonListener() );
 
         JLabel text = new JLabel( "Quick Quiz Generator" );
         text.setFont( new Font( "font", Font.PLAIN, 48 ) );
         text.setHorizontalAlignment( JLabel.CENTER );
-        text.setBounds( 100, 30, 600, 100 );
+        text.setBounds( 100, 30, 600, 75 );
+        
 
-        frame = new JFrame( "MainMenu" );
+        frame = new JFrame( "Main Menu" );
         frame.setDefaultCloseOperation( JFrame.EXIT_ON_CLOSE );
         Container c = frame.getContentPane();
         frame.setLayout( null );
@@ -44,6 +49,7 @@ public class MainMenu
         c.add( trainMenu );
         c.add( addMenu );
         c.add( removeMenu );
+        c.add(statsMenu);
         c.add( text );
 
         frame.setResizable( false );
@@ -87,6 +93,15 @@ public class MainMenu
         public void actionPerformed( ActionEvent e )
         {
             RemoveMenu removeMenu = new RemoveMenu( problem );
+            frame.dispose();
+        }
+    }
+    
+    private class StatButtonListener implements ActionListener
+    {
+        public void actionPerformed( ActionEvent e )
+        {
+            StatsMenu statsMenu = new StatsMenu();
             frame.dispose();
         }
     }
