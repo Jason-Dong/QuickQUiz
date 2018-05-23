4a32733b84303f6767ac3dc646440e12a2541cb1
krishnazax <30795196+krishnazax@users.noreply.github.com>
2018-05-23 16:56:58 -0700

    Add files via upload

--- StartMenu.java
+++ StartMenu.java
@@ -8,6 +8,18 @@ import javax.swing.JFileChooser;
 import javax.swing.filechooser.FileSystemView;
 
 
+/**
+ * This class is the Start Menu, which asks for the location of the directory
+ * which contains the program files and the image and problem files to set the 
+ * space in which the code functions and accesses problems.
+ *
+ * @author Krishnkumar Bhattaram
+ * @version May 23, 2018
+ * @author Period: 2
+ * @author Assignment: QuickQuiz
+ *
+ * @author Sources: none
+ */
 public class StartMenu
 {
     /**
@@ -34,26 +46,29 @@ public class StartMenu
      * The text label to warn that the name is not in problem
      */
     private JLabel notfoundtxt;
-    
+
     /**
      * The statistics from this set
      */
     private Statistics stats;
-    
+
     private String pathFileChooser;
 
+
     /**
-     * The constructor, which makes visible the frame and adds the enter button and
-     * the single text field for the pathname of the folder in which the program is located.
+     * The constructor, which makes visible the frame and adds the enter button
+     * and the single text field for the pathname of the folder in which the
+     * program is located.
+     * 
      * @param statistics
-     *          The statistics for this set (from QuickQuiz)
+     *            The statistics for this set (from QuickQuiz)
      */
-    public StartMenu(Statistics statistics)
+    public StartMenu( Statistics statistics )
     {
         stats = statistics;
-        
+
         JLabel nametxt = new JLabel(
-            "Enter the location of the program file to begin (ex. /Users/JohnDoe/Desktop/QuickQuiz/)" );
+            "Enter the location of the program file to begin (ex. /Users/JohnDoe/Desktop/QuickQuiz)" );
         nametxt.setFont( new Font( "font", Font.PLAIN, 10 ) );
         nametxt.setBounds( 200, 180, 500, 20 );
         nametxt.setHorizontalAlignment( JLabel.LEFT );
@@ -62,11 +77,11 @@ public class StartMenu
         namepathfield.setBounds( 200, 200, 400, 20 );
 
         JButton pathname = new JButton( "Enter" );
-        pathname.setBounds( 100, 200, 50, 20 );
+        pathname.setBounds( 600, 200, 100, 20 );
         pathname.addActionListener( new PathnameListener() );
-        
+
         JButton filechooser = new JButton( "Choose File" );
-        filechooser.setBounds( 600, 200, 100, 20 );
+        filechooser.setBounds( 100, 200, 100, 20 );
         filechooser.addActionListener( new FileListener() );
 
         JLabel text = new JLabel( "Welcome to Quick Quiz!" );
@@ -94,12 +109,12 @@ public class StartMenu
         notfoundtxt.setVerticalAlignment( JLabel.TOP );
         notfoundtxt.setVisible( false );
 
-        namepathfield.setText( "/Users/JohnDoe/Desktop/QuickQuiz/" );
+        namepathfield.setText( "/Users/JohnDoe/Desktop/QuickQuiz" );
         c.add( text );
         c.add( pathname );
         c.add( warntxt );
         c.add( notfoundtxt );
-        c.add(filechooser);
+        c.add( filechooser );
 
         c.add( namepathfield );
         c.add( nametxt );
@@ -108,10 +123,12 @@ public class StartMenu
         frame.setVisible( true );
     }
 
+
     /**
-     * Handles when the enter button is clicked; when clicked, checks if all conditions are 
-     * satisfied (if all fields are entered and all pathnames exist) and displays the appropriate
-     * warnings if not. Otherwise, the frame closes itself and opens MainMenu.
+     * Handles when the enter button is clicked; when clicked, checks if all
+     * conditions are satisfied (if all fields are entered and all pathnames
+     * exist) and displays the appropriate warnings if not. Otherwise, the frame
+     * closes itself and opens MainMenu.
      *
      * @author Krishnakumar Bhattaram
      * @version May 19, 2018
@@ -150,20 +167,34 @@ public class StartMenu
             }
         }
     }
-    
+
+
+    /**
+     * Opens the system's file explorer to allow selection of the directory which 
+     * contains the image files and problem set.
+     *
+     * @author Krishnakumar Bhattaram
+     * @version May 23, 2018
+     * @author Period: 2
+     * @author Assignment: QuickQuiz
+     *
+     * @author Sources: Mykong.com
+     */
     private class FileListener implements ActionListener
     {
-            public void actionPerformed( ActionEvent e)
-            {
-                JFileChooser jfc = new JFileChooser(FileSystemView.getFileSystemView().getHomeDirectory());
-                jfc.setFileSelectionMode(JFileChooser.DIRECTORIES_ONLY);
-                int returnValue = jfc.showOpenDialog(null);
+        public void actionPerformed( ActionEvent e )
+        {
+            JFileChooser filechooser = new JFileChooser(
+                FileSystemView.getFileSystemView().getHomeDirectory() );
+            filechooser.setFileSelectionMode( JFileChooser.DIRECTORIES_ONLY );
+            int ret = filechooser.showOpenDialog( null );
 
-                if (returnValue == JFileChooser.APPROVE_OPTION) {
-                    File selectedFile = jfc.getSelectedFile();
-                    pathFileChooser = selectedFile.getAbsolutePath();
-                    namepathfield.setText(pathFileChooser);
-                }
+            if ( ret == JFileChooser.APPROVE_OPTION )
+            {
+                File file = filechooser.getSelectedFile();
+                pathFileChooser = file.getAbsolutePath();
+                namepathfield.setText( pathFileChooser );
             }
+        }
     }
 }
