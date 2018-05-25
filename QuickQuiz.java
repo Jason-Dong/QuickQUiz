11c957198478655595a0110d2a9c3e10498700eb
krishnazax <30795196+krishnazax@users.noreply.github.com>
2018-05-25 15:23:55 -0700

    Add files via upload

--- QuickQuiz.java
+++ QuickQuiz.java
@@ -1,8 +1,17 @@
+import javax.swing.JFrame;
+
 public class QuickQuiz
 {
     public static void main( String[] args )
     {
+        JFrame frame = new JFrame( "Fîzîk" );
+        frame.setDefaultCloseOperation( JFrame.EXIT_ON_CLOSE );
+        frame.setLayout( null );
+        frame.setBounds( 0, 0, 800, 600 );
+        frame.setResizable( false );
+        frame.setVisible( true );
+        
         Statistics progressKeeper = new Statistics();
-        StartMenu start = new StartMenu( progressKeeper );
+        StartMenu start = new StartMenu( progressKeeper, frame );
     }
 }
