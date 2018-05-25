49fed96dc191392dd9f5ec8f41b543301a3d4100
cobaltmilotic <1austinlei@gmail.com>
2018-05-25 09:17:55 -0700

    Add files via upload

--- AddMenu.java
+++ AddMenu.java
@@ -9,7 +9,7 @@ import javax.swing.filechooser.FileSystemView;
 
 
 /**
- * Allows the user to add problems, checking each field to ensure that all 
+ * Allows the user to add problems, checking each field to ensure that all
  * fields are completed and the file pathnames are valid. It also ensures that
  * no two problems are added with the same name.
  *
@@ -22,141 +22,144 @@ import javax.swing.filechooser.FileSystemView;
  */
 public class AddMenu
 {
-	/**
-	 * Holds the GUI elements
-	 */
+    /**
+     * Holds the GUI elements
+     */
     private JFrame frame;
 
     /**
-	 * The problem database for this set
-	 */
+     * The problem database for this set
+     */
     private ProblemDatabase problem;
 
     /**
-	 * The text field for the problem pathname
-	 */
+     * The text field for the problem pathname
+     */
     private JTextField problempathfield;
 
     /**
-	 * The text field for the solution pathname
-	 */
+     * The text field for the solution pathname
+     */
     private JTextField solutionpathfield;
 
     /**
-	 * The text field for the problem name
-	 */
+     * The text field for the problem name
+     */
     private JTextField namepathfield;
 
     /**
-	 * Type of problem to add chosen by the buttons
-	 */
+     * Type of problem to add chosen by the buttons
+     */
     private int type;
 
     /**
-	 * The button to select problem type as kinematics
-	 */
+     * The button to select problem type as kinematics
+     */
     private JButton kinematics;
 
     /**
-	 * The button to select problem type as newton's laws
-	 */
+     * The button to select problem type as newton's laws
+     */
     private JButton newton;
 
     /**
-	 * The button to select problem type as energy
-	 */
+     * The button to select problem type as energy
+     */
     private JButton energy;
 
     /**
-	 * The button to select problem type as rotation
-	 */
+     * The button to select problem type as rotation
+     */
     private JButton rotation;
 
     /**
-	 * The button to select problem type as gravitation
-	 */
+     * The button to select problem type as gravitation
+     */
     private JButton gravitation;
 
     /**
-	 * The button to select problem type as fluids
-	 */
+     * The button to select problem type as fluids
+     */
     private JButton fluids;
 
     /**
-	 * The button to select problem solution as 'a'
-	 */
+     * The button to select problem solution as 'a'
+     */
     private JButton ap;
 
     /**
-	 * The button to select problem solution as 'b'
-	 */
+     * The button to select problem solution as 'b'
+     */
     private JButton bp;
 
     /**
-	 * The button to select problem solution as 'c'
-	 */
+     * The button to select problem solution as 'c'
+     */
     private JButton cp;
 
     /**
-	 * The button to select problem solution as 'd'
-	 */
+     * The button to select problem solution as 'd'
+     */
     private JButton dp;
 
     /**
-	 * The button to select problem solution as 'e'
-	 */
+     * The button to select problem solution as 'e'
+     */
     private JButton ep;
 
     /**
-	 * The answer as chosen by the buttons
-	 */
+     * The answer as chosen by the buttons
+     */
     private char ans;
 
     /**
-	 * The container of the frame
-	 */
+     * The container of the frame
+     */
     private Container c;
 
     /**
-	 * The text label to warn that the fields are not completed
-	 */
+     * The text label to warn that the fields are not completed
+     */
     private JLabel warntxt;
 
     /**
-	 * The text label to indicate that a new problem was added
-	 */
+     * The text label to indicate that a new problem was added
+     */
     private JLabel newtxt;
 
     /**
-	 * The text label to warn that the problem name is already used
-	 */
+     * The text label to warn that the problem name is already used
+     */
     private JLabel existtxt;
-    
-    /**
-	 * The text label to warn that the problem pathname doesn't exist
-	 */
-    private JLabel probnotfoundtxt;
-    
-    /**
-	 * The text label to warn that the solution pathname doesn't exist
-	 */
-    private JLabel solnotfoundtxt;
-    
-    /**
-	 * The statistics from the menu
-	 */
-    private Statistics stats;
 
     /**
-     * The constructor, which makes visible the frame and adds the buttons and text fields
-     * @param problem	
-     * 			The problem database for this set
+     * The text label to warn that the problem pathname doesn't exist
+     */
+    private JLabel probnotfoundtxt;
+
+    /**
+     * The text label to warn that the solution pathname doesn't exist
+     */
+    private JLabel solnotfoundtxt;
+
+    /**
+     * The statistics from the menu
+     */
+    private Statistics stats;
+
+
+    /**
+     * The constructor, which makes visible the frame and adds the buttons and
+     * text fields
+     * 
+     * @param problem
+     *            The problem database for this set
      * @param statistics
-     * 			The statistics for this set
+     *            The statistics for this set
      */
     public AddMenu( ProblemDatabase problem, Statistics statistics )
     {
-    		stats = statistics;
+        stats = statistics;
         ans = 'h';
         type = -1;
         this.problem = problem;
@@ -172,7 +175,7 @@ public class AddMenu
         problemtxt.setVerticalAlignment( problemtxt.TOP );
         problempathfield = new JTextField();
         problempathfield.setBounds( 200, 200, 400, 20 );
-        
+
         JButton chooseproblem = new JButton( "Choose File" );
         chooseproblem.setBounds( 600, 200, 100, 20 );
         chooseproblem.addActionListener( new ProblemFileListener() );
@@ -184,7 +187,7 @@ public class AddMenu
         solutiontxt.setVerticalAlignment( solutiontxt.TOP );
         solutionpathfield = new JTextField();
         solutionpathfield.setBounds( 200, 240, 400, 20 );
-        
+
         JButton choosesolution = new JButton( "Choose File" );
         choosesolution.setBounds( 600, 240, 100, 20 );
         choosesolution.addActionListener( new SolutionFileListener() );
@@ -281,21 +284,21 @@ public class AddMenu
         newtxt.setHorizontalAlignment( warntxt.LEFT );
         newtxt.setVerticalAlignment( warntxt.TOP );
         newtxt.setVisible( false );
-        
+
         existtxt = new JLabel( "Another problem already has this name!" );
         existtxt.setFont( new Font( "font", Font.PLAIN, 15 ) );
         existtxt.setBounds( 200, 500, 500, 20 );
         existtxt.setHorizontalAlignment( existtxt.LEFT );
         existtxt.setVerticalAlignment( existtxt.TOP );
         existtxt.setVisible( false );
-        
+
         probnotfoundtxt = new JLabel( "Problem file not found! Please check the fields" );
         probnotfoundtxt.setFont( new Font( "font", Font.PLAIN, 15 ) );
         probnotfoundtxt.setBounds( 200, 500, 500, 20 );
         probnotfoundtxt.setHorizontalAlignment( JLabel.LEFT );
         probnotfoundtxt.setVerticalAlignment( JLabel.TOP );
         probnotfoundtxt.setVisible( false );
-        
+
         solnotfoundtxt = new JLabel( "Solution file not found! Please check the fields" );
         solnotfoundtxt.setFont( new Font( "font", Font.PLAIN, 15 ) );
         solnotfoundtxt.setBounds( 200, 500, 500, 20 );
@@ -308,9 +311,9 @@ public class AddMenu
         c.add( pathname );
         c.add( warntxt );
         c.add( newtxt );
-        c.add(solnotfoundtxt);
-        c.add(existtxt);
-        c.add(probnotfoundtxt);
+        c.add( solnotfoundtxt );
+        c.add( existtxt );
+        c.add( probnotfoundtxt );
 
         c.add( problempathfield );
         c.add( problemtxt );
@@ -340,6 +343,7 @@ public class AddMenu
         frame.setVisible( true );
     }
 
+
     /**
      * Handles when the back button is clicked; when clicked, returns to
      * MainMenu by closing all of TypeMenu and creating a MainMenu object.
@@ -362,9 +366,9 @@ public class AddMenu
 
 
     /**
-     * Handles when the enter button is clicked; when clicked, checks if all conditions are 
-     * satisfied (if all fields are entered and all pathnames exist) and displays the appropriate
-     * warnings/successes
+     * Handles when the enter button is clicked; when clicked, checks if all
+     * conditions are satisfied (if all fields are entered and all pathnames
+     * exist) and displays the appropriate warnings/successes
      *
      * @author Krishnakumar Bhattaram
      * @version May 19, 2018
@@ -382,12 +386,12 @@ public class AddMenu
             {
                 newtxt.setVisible( false );
                 warntxt.setVisible( true );
-                existtxt.setVisible(false);
-                probnotfoundtxt.setVisible(false);
-                solnotfoundtxt.setVisible(false);
+                existtxt.setVisible( false );
+                probnotfoundtxt.setVisible( false );
+                solnotfoundtxt.setVisible( false );
                 return;
             }
-            
+
             String pathproblems = problempathfield.getText();
             String solutionpath = solutionpathfield.getText();
             try
@@ -396,36 +400,36 @@ public class AddMenu
             }
             catch ( FileNotFoundException exc )
             {
-            		newtxt.setVisible( false );
+                newtxt.setVisible( false );
                 warntxt.setVisible( false );
-                existtxt.setVisible(false);
-                probnotfoundtxt.setVisible(true);
-                solnotfoundtxt.setVisible(false);
+                existtxt.setVisible( false );
+                probnotfoundtxt.setVisible( true );
+                solnotfoundtxt.setVisible( false );
                 return;
             }
-            
+
             try
             {
                 Scanner readIn = new Scanner( new File( solutionpath ) );
             }
             catch ( FileNotFoundException exc )
             {
-            		newtxt.setVisible( false );
+                newtxt.setVisible( false );
                 warntxt.setVisible( false );
-                existtxt.setVisible(false);
-                probnotfoundtxt.setVisible(false);
-                solnotfoundtxt.setVisible(true);
+                existtxt.setVisible( false );
+                probnotfoundtxt.setVisible( false );
+                solnotfoundtxt.setVisible( true );
                 return;
             }
-            
-            if (!problem.addProblem(new Problem( problempathfield
-                    .getText(), solutionpathfield.getText(), namepathfield.getText(), type, ans)))
+
+            if ( !problem.addProblem( new Problem( problempathfield
+                .getText(), solutionpathfield.getText(), namepathfield.getText(), type, ans ) ) )
             {
-            	    newtxt.setVisible( false );
+                newtxt.setVisible( false );
                 warntxt.setVisible( false );
-                existtxt.setVisible(true);
-                probnotfoundtxt.setVisible(false);
-                solnotfoundtxt.setVisible(false);
+                existtxt.setVisible( true );
+                probnotfoundtxt.setVisible( false );
+                solnotfoundtxt.setVisible( false );
             }
             else
             {
@@ -446,18 +450,20 @@ public class AddMenu
                 ans = 'h';
                 type = -1;
                 warntxt.setVisible( false );
-                existtxt.setVisible(false);
+                existtxt.setVisible( false );
                 newtxt.setVisible( true );
-                probnotfoundtxt.setVisible(false);
-                solnotfoundtxt.setVisible(false);
+                probnotfoundtxt.setVisible( false );
+                solnotfoundtxt.setVisible( false );
             }
         }
     }
 
+
     /**
-     * Handles when one of the type buttons (ex. kinematics, energy, etc.)
-     * are clicked; when clicked, changes the type field to match the topic chosen and
-     * disables the chosen type button while enabling all other type buttons.
+     * Handles when one of the type buttons (ex. kinematics, energy, etc.) are
+     * clicked; when clicked, changes the type field to match the topic chosen
+     * and disables the chosen type button while enabling all other type
+     * buttons.
      *
      * @author Krishnakumar Bhattaram
      * @version May 24, 2018
@@ -478,52 +484,54 @@ public class AddMenu
             rotation.setEnabled( true );
             gravitation.setEnabled( true );
             fluids.setEnabled( true );
-            
-            if(name == null)
+
+            if ( name == null )
             {
                 warntxt.setVisible( true );
                 newtxt.setVisible( false );
                 System.out.println( "Fatal Error!" );
                 return;
             }
-            
-            if(name.equalsIgnoreCase( "Kinematics" ))
+
+            if ( name.equalsIgnoreCase( "Kinematics" ) )
             {
                 kinematics.setEnabled( false );
                 type = 0;
             }
-            else if(name.equalsIgnoreCase( "Newton's Laws" ))
+            else if ( name.equalsIgnoreCase( "Newton's Laws" ) )
             {
                 newton.setEnabled( false );
                 type = 1;
             }
-            else if(name.equalsIgnoreCase( "Energy" ))
+            else if ( name.equalsIgnoreCase( "Energy" ) )
             {
                 energy.setEnabled( false );
                 type = 2;
             }
-            else if(name.equalsIgnoreCase( "Rotation" ))
+            else if ( name.equalsIgnoreCase( "Rotation" ) )
             {
                 rotation.setEnabled( false );
                 type = 3;
             }
-            else if(name.equalsIgnoreCase( "Gravitation" ))
+            else if ( name.equalsIgnoreCase( "Gravitation" ) )
             {
                 gravitation.setEnabled( false );
                 type = 4;
             }
-            else if(name.equalsIgnoreCase( "Fluids" ))
+            else if ( name.equalsIgnoreCase( "Fluids" ) )
             {
                 fluids.setEnabled( false );
                 type = 5;
             }
         }
     }
-    
+
+
     /**
-     * Handles when one of the answer buttons (ex. 'A', 'B', etc.)
-     * are clicked; when clicked, changes the answer field to match the topic chosen and
-     * disables the chosen answer button while enabling all other answer buttons.
+     * Handles when one of the answer buttons (ex. 'A', 'B', etc.) are clicked;
+     * when clicked, changes the answer field to match the topic chosen and
+     * disables the chosen answer button while enabling all other answer
+     * buttons.
      *
      * @author Krishnakumar Bhattaram
      * @version May 24, 2018
@@ -543,53 +551,56 @@ public class AddMenu
             cp.setEnabled( true );
             dp.setEnabled( true );
             ep.setEnabled( true );
-            
-            if(name == null)
+
+            if ( name == null )
             {
                 warntxt.setVisible( true );
                 newtxt.setVisible( false );
                 System.out.println( "Fatal Error!" );
                 return;
             }
-            
-            if(name.equalsIgnoreCase( "A" ))
+
+            if ( name.equalsIgnoreCase( "A" ) )
             {
                 ap.setEnabled( false );
-                ans = 'a';
+                ans = 'A';
             }
-            else if(name.equalsIgnoreCase( "B" ))
+            else if ( name.equalsIgnoreCase( "B" ) )
             {
                 bp.setEnabled( false );
-                ans = 'b';
+                ans = 'B';
             }
-            else if(name.equalsIgnoreCase( "C" ))
+            else if ( name.equalsIgnoreCase( "C" ) )
             {
                 cp.setEnabled( false );
-                ans = 'c';
+                ans = 'C';
             }
-            else if(name.equalsIgnoreCase( "D" ))
+            else if ( name.equalsIgnoreCase( "D" ) )
             {
                 dp.setEnabled( false );
-                ans = 'd';
+                ans = 'D';
             }
-            else if(name.equalsIgnoreCase( "E" ))
+            else if ( name.equalsIgnoreCase( "E" ) )
             {
                 ep.setEnabled( false );
-                ans = 'e';
+                ans = 'E';
             }
         }
     }
-    
+
+
     /**
-     * Opens the system's file explorer to allow selection of the directory which 
-     * contains the image files and problem set. Only allows selection of PNG files.
+     * Opens the system's file explorer to allow selection of the directory
+     * which contains the image files and problem set. Only allows selection of
+     * PNG files.
      *
      * @author Krishnakumar Bhattaram
      * @version May 23, 2018
      * @author Period: 2
      * @author Assignment: QuickQuiz
      *
-     * @author Sources: https://docs.oracle.com/javase/7/docs/api/javax/swing/JFileChooser.html
+     * @author Sources:
+     *         https://docs.oracle.com/javase/7/docs/api/javax/swing/JFileChooser.html
      */
     private class ProblemFileListener implements ActionListener
     {
@@ -597,9 +608,9 @@ public class AddMenu
         {
             JFileChooser filechooser = new JFileChooser(
                 FileSystemView.getFileSystemView().getHomeDirectory() );
-            filechooser.setDialogTitle("Choose Problem File");
+            filechooser.setDialogTitle( "Choose Problem File" );
             filechooser.setAcceptAllFileFilterUsed( false );
-            FileNameExtensionFilter types = new FileNameExtensionFilter("PNG images", "png");
+            FileNameExtensionFilter types = new FileNameExtensionFilter( "PNG images", "png" );
             filechooser.addChoosableFileFilter( types );
             int ret = filechooser.showOpenDialog( null );
 
@@ -610,17 +621,20 @@ public class AddMenu
             }
         }
     }
-    
+
+
     /**
-     * Opens the system's file explorer to allow selection of the directory which 
-     * contains the image files and problem set. Only allows selection of PNG files.
+     * Opens the system's file explorer to allow selection of the directory
+     * which contains the image files and problem set. Only allows selection of
+     * PNG files.
      *
      * @author Krishnakumar Bhattaram
      * @version May 23, 2018
      * @author Period: 2
      * @author Assignment: QuickQuiz
      *
-     * @author Sources: https://docs.oracle.com/javase/7/docs/api/javax/swing/JFileChooser.html
+     * @author Sources:
+     *         https://docs.oracle.com/javase/7/docs/api/javax/swing/JFileChooser.html
      */
     private class SolutionFileListener implements ActionListener
     {
@@ -628,9 +642,9 @@ public class AddMenu
         {
             JFileChooser filechooser = new JFileChooser(
                 FileSystemView.getFileSystemView().getHomeDirectory() );
-            filechooser.setDialogTitle("Choose Solution File");
+            filechooser.setDialogTitle( "Choose Solution File" );
             filechooser.setAcceptAllFileFilterUsed( false );
-            FileNameExtensionFilter types = new FileNameExtensionFilter("PNG images", "png");
+            FileNameExtensionFilter types = new FileNameExtensionFilter( "PNG images", "png" );
             filechooser.addChoosableFileFilter( types );
             int ret = filechooser.showOpenDialog( null );
 
