63cc7ea4de91a7af6da702985dff5ee62ca5033a
Jason-Dong <jdong120@gmail.com>
2018-05-30 13:23:50 -0700

    Add files via upload

--- QuickQuiz.java
+++ QuickQuiz.java
@@ -1,5 +1,20 @@
+/**
+ * This is the initial class which starts the entire program.
+ *
+ * @author Jason
+ * @version May 30, 2018
+ * @author Period: 2
+ * @author Assignment: QuickQuiz
+ *
+ * @author Sources: none
+ */
 public class QuickQuiz
 {
+    /**
+     * Calls the StartMenu which starts the program
+     * 
+     * @param args
+     */
     public static void main( String[] args )
     {
         Statistics progressKeeper = new Statistics();
